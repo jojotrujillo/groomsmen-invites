4085d50b7ead87a235b20f7acaf5fb156c217982
Jordon Trujillo <jordontrujillo@outlook.com>
2023-02-11 04:22:54 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Groomsmen Invites`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
